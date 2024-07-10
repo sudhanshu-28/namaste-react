9dde524dadd938089b39f713944c5c23b4a23114
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-07-11 01:42:06 +0530

    Food Cart Application. #1

--- App.js
+++ App.js
@@ -1,48 +1,83 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element
-const heading = React.createElement(
-  "h1",
-  { id: "header", style: { color: "blue" } },
-  "Namaste React Heading"
-);
+import { restaurantsList } from "./mock";
 
-// React Component - Class based Component, Functional Component
+import "./index.css";
 
-// Arrow function
-// const Title = () => {
-//   return <h1 id="header">Namaste React</h1>;
-// };
-
-const element = <span>React Element</span>;
-
-// Normal function
-function Title() {
-  return (
-    <div>
-      <h1 id="header">Namaste React Title</h1>
-      {element}
-    </div>
-  );
-}
-
-// Component Composition => its to call one component inside another component
 const Header = () => {
   return (
-    <div className="container">
-      {Title()}
-      <h1 className="heading">Header component</h1>
+    <div className="app-header">
+      <div className="logo-container">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/004/312/696/original/food-logo-template-design-icon-illustration-vector.jpg"
+          alt="Food Logo"
+          width={180}
+          height={120}
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 };
 
-// JSX => Transpiled to => React.createElement(object) => renderHTML(render)
-const jsxHeading = <h1>Namaste React from JSX</h1>;
+const RestaurantCard = (props) => {
+  const { restaurant } = props;
+
+  const { cloudinaryImageId, name, avgRatingString, cuisines, locality, sla } =
+    restaurant?.info;
+
+  const imageUrl = `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`;
+
+  return (
+    <div className="restaurant-card">
+      <img src={imageUrl} alt={name} className="restuarant-logo" />
+      <h3>{name}</h3>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <h4>{`${avgRatingString} stars`}</h4>
+        <h4>{sla?.slaString}</h4>
+      </div>
+      <div>
+        <span>{cuisines.join(", ")}</span>
+        <br />
+        <span>{locality}</span>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="app-body">
+      <div className="search-container">
+        <input type="search" name="" id="" />
+        <button type="submit">Search</button>
+      </div>
+      <div className="restaurant-container">
+        {restaurantsList.map((el) => (
+          <RestaurantCard key={el?.info?.id} restaurant={el} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// root.render(heading); // Display React element
-// root.render(jsxHeading); // Display React JSX
-
-root.render(<Header />); // Display React Component
+root.render(<AppLayout />);
