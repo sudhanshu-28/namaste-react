dc00fecf0a3a8f618305dd829c4625a2c1bb8f38
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-07-26 01:06:08 +0530

    Swiggy Live API Integration in App.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import RestaurantCard from "./RestaurantCard";
+
 import { restaurantsList } from "../utils/mockData";
+import { SWIGGY_API_ENDPOINT } from "../utils/constants";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurant] = useState(restaurantsList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const data = await fetch(SWIGGY_API_ENDPOINT);
+      const json = await data.json();
+      const restaurants =
+        json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants || [];
+      setListOfRestaurant(restaurants);
+    } catch (err) {}
+  };
 
   return (
     <div className="app-body">
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,3 +3,6 @@ export const CDN_URL =
 
 export const LOGO_URL =
   "https://static.vecteezy.com/system/resources/previews/004/312/696/original/food-logo-template-design-icon-illustration-vector.jpg";
+
+export const SWIGGY_API_ENDPOINT = `
+https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.07480&lng=72.88560&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`;
