a3e2a02c88b71c9c50669dfa13df12ee7e2c966e
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-11-10 16:56:34 +0530

    Lazy Load route pages & components which are heavy.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+  useActionData,
+} from "react-router-dom";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -9,11 +14,17 @@ import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
 import Shimmer from "./components/Shimmer";
 
-// When our App grows, we cana divide our app based on functionality into chunks and lazy load
+// When our App grows, we can divide our app based on functionality into chunks and lazy load
 // To reduce this Bundle size, we do code splitting, do this chunking, do this lazy loading
 // Dynamic Import
+// This all keys words - Chunking . Code Splitting / Dynamic Bundling / Lazy Loading - all are same thing
+// Lazy Loading is also known as on-demand loading
+
 const Grocery = lazy(() => import("./components/Grocery"));
 const About = lazy(() => import("./components/About"));
+
+// lazy is the named export provided by react to useActionData
+// it takes callback function with import method which takes component path as argument
 
 const AppLayout = () => {
   return (
