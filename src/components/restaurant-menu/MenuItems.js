23f223a68fdc8738301ac16667842e73050e9aa0
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-11-13 00:17:59 +0530

    Menu Item Pricing fixed.

--- src/components/restaurant-menu/MenuItems.js
+++ src/components/restaurant-menu/MenuItems.js
@@ -4,7 +4,9 @@ import { CDN_URL } from "../../utils/constants";
 const ItemLeftContainer = ({ item }) => (
   <div className="flex-[7] space-y-1">
     <div className="font-bold text-gray-600 text-lg">{item?.name}</div>
-    <div className="text-gray-900 font-bold">{`₨.${item?.price / 100}`}</div>
+    <div className="text-gray-900 font-bold">{`₨.${
+      (item?.price || item?.defaultPrice) / 100
+    }`}</div>
     {item?.ratings?.aggregatedRating?.rating && (
       <div className="text-sm font-bold space-x-1">
         <span className="text-green-800">{`⁕ ${item?.ratings?.aggregatedRating?.rating}`}</span>
