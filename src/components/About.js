5f259cff9346eeed69abc4ec934dd33bb9a3e27a
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-11-16 14:05:08 +0530

    use Context.Consume. (Another way to access Context). Old way - works fine with Class Components.

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 
 import User from "./User";
+import UserContext from "../utils/UserContext";
 
 class About extends Component {
   constructor(props) {
@@ -13,6 +14,9 @@ class About extends Component {
     return (
       <div>
         <h1>About Page</h1>
+        <UserContext.Consumer>
+          {({ loggedInUser }) => <h2>{loggedInUser}</h2>}
+        </UserContext.Consumer>
         <User />
       </div>
     );
