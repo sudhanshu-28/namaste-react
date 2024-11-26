1f270dff08586d17c9912bc8cd30aab16b2f6b4d
Sudhanshu Rai <sudhanshurai97@gmail.com>
2024-11-26 11:28:51 +0530

    This is Unit Testing.

--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -2,60 +2,62 @@ import { render, screen } from "@testing-library/react";
 import Contact from "../Contact";
 import "@testing-library/jest-dom";
 
-test("Should load Contact component", () => {
-  // To test component we need to render the component on jest-dom first
-  render(<Contact />);
+describe("Contact Us page Test Case", () => {
+  test("Should load Contact component", () => {
+    // To test component we need to render the component on jest-dom first
+    render(<Contact />);
 
-  const heading = screen.getByRole("heading");
+    const heading = screen.getByRole("heading");
 
-  // Assertion
-  expect(heading).toBeInTheDocument();
-});
+    // Assertion
+    expect(heading).toBeInTheDocument();
+  });
 
-test("Should load button inside Contact component", () => {
-  render(<Contact />);
+  test("Should load button inside Contact component", () => {
+    render(<Contact />);
 
-  const btn = screen.getByRole("button");
+    const btn = screen.getByRole("button");
 
-  // Assertion
-  expect(btn).toBeInTheDocument();
-});
+    // Assertion
+    expect(btn).toBeInTheDocument();
+  });
 
-test("Should exist Button with label Submit", () => {
-  render(<Contact />);
+  test("Should exist Button with label Submit", () => {
+    render(<Contact />);
 
-  const btnLabel = screen.getByText("Submit");
+    const btnLabel = screen.getByText("Submit");
 
-  // Assertion
-  expect(btnLabel).toBeInTheDocument();
-});
+    // Assertion
+    expect(btnLabel).toBeInTheDocument();
+  });
 
-test("Should exist Button with label Submit", () => {
-  render(<Contact />);
+  test("Should exist Button with label Submit", () => {
+    render(<Contact />);
 
-  const btnLabel = screen.getByText("Submit");
+    const btnLabel = screen.getByText("Submit");
 
-  // Assertion
-  expect(btnLabel).toBeInTheDocument();
-});
+    // Assertion
+    expect(btnLabel).toBeInTheDocument();
+  });
 
-test("Should load input name inside Contact component", () => {
-  render(<Contact />);
+  test("Should load input name inside Contact component", () => {
+    render(<Contact />);
 
-  const nameInputField = screen.getByPlaceholderText("Enter name");
+    const nameInputField = screen.getByPlaceholderText("Enter name");
 
-  // Assertion
-  expect(nameInputField).toBeInTheDocument();
-});
+    // Assertion
+    expect(nameInputField).toBeInTheDocument();
+  });
 
-test("Should load 3 input boxes on Contact component", () => {
-  // 1. Render something
-  render(<Contact />);
+  it("Should load 3 input boxes on Contact component", () => {
+    // 1. Render something
+    render(<Contact />);
 
-  // 2. Query something
-  const inputBoxes = screen.getAllByRole("textbox");
-  console.log("inputBoxes => ", inputBoxes.length);
+    // 2. Query something
+    const inputBoxes = screen.getAllByRole("textbox");
+    console.log("inputBoxes => ", inputBoxes.length);
 
-  // 3. Assertion something
-  expect(inputBoxes.length).toBe(3);
+    // 3. Assertion something
+    expect(inputBoxes.length).toBe(3);
+  });
 });
